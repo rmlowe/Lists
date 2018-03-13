096ac83246a0e2d9ebca4231960ab8919cc39496
Robert Lowe <robertmlowe@rmlowe.com>
2018-03-13 12:39:15 +0800

    Initial commit

---
+++ src/academy/learnprogramming/lists/Lists.kt
@@ -0,0 +1,8 @@
+package academy.learnprogramming.lists
+
+
+fun main(args: Array<String>) {
+
+    val strings = listOf("spring", "summer", "fall", "winter")
+    val colorList = listOf("black", "white", "red")
+}
